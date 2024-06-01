9381c2618fd8710a0189c8da987a48488dad1cfb
Pandemoniusly <157427865+Pandemoniusly@users.noreply.github.com>
2024-06-01 15:51:19 -0400

    Add project files.

---
+++ hivebombnetcode/ExplosionHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace hivebombnetcode
+{
+    internal class ExplosionHandler
+    {
+        public static void ExplodeAt(Transform __transform,int rand)
+        {
+            if (Config.Instance.Enabled.Value == true)
+            {
+                if (Config.Instance.KnockbackEnabled.Value == true)
+                {
+                    Landmine.SpawnExplosion(__transform.position, Config.Instance.VisibleExplosions.Value, 0f, Config.Instance.Radius.Value, Config.Instance.MaxPlayerDamage.Value, rand);
+                }
+                else
+                {
+                    Landmine.SpawnExplosion(__transform.position, Config.Instance.VisibleExplosions.Value, 0f, Config.Instance.Radius.Value, Config.Instance.MaxPlayerDamage.Value, 0);
+                }
+            }
+        }
+    }
+}
