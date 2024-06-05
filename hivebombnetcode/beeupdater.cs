fb337d9ca59f9475a0e8f89fd6b616586b67e868
Pandemoniusly <157427865+Pandemoniusly@users.noreply.github.com>
2024-06-04 20:33:16 -0400

    Update beeupdater.cs

--- hivebombnetcode/beeupdater.cs
+++ hivebombnetcode/beeupdater.cs
@@ -1,11 +1,30 @@
 using HarmonyLib;
-using System;
+using GameNetcodeStuff;
+using RuntimeNetcodeRPCValidator;
+using Unity.Netcode;
+using UnityEngine;
+using Random = System.Random;
 
 namespace hivebombnetcode
 {
     [HarmonyPatch(typeof(GrabbableObject))]
     internal class beeupdate
     {
+        [SerializeField]
+        public static bool hasupdated = false;
+        [SerializeField]
+        public static bool knockback = false;
+        [SerializeField]
+        public static bool visible = false;
+        [SerializeField]
+        public static Vector3 where = new Vector3();
+        [SerializeField]
+        public static int rand = 0;
+        [SerializeField]
+        public static int dmg = 0;
+        [SerializeField]
+        public static float radius = 0;
+
         public static int Framecount = 0;
         public static readonly Random getrandom = new Random();
 
@@ -13,20 +32,34 @@ namespace hivebombnetcode
         [HarmonyPrefix]
         public static void beehiveexplode(GrabbableObject __instance)
         {
-            if (__instance.name == "RedLocustHive(Clone)")
+            if (GameNetworkManager.Instance.localPlayerController.isHostPlayerObject)
             {
-                //finalhivebomb.Logger.LogInfo("found");
-                if (Framecount <= 0)
+                if (Config.Instance.Enabled.Value == true & !hasupdated)
                 {
-                    Framecount = 10;
-                    if ((getrandom.Next(800) <= getrandom.Next(10)))
+
+                    if (__instance.name == "RedLocustHive(Clone)")
                     {
-                        hivebombnetcode.ExplosionHandler.ExplodeAt(__instance.transform, getrandom.Next(30));
+
+                        //finalhivebomb.Logger.LogInfo("found");
+                        if (Framecount <= 0)
+                        {
+                            Framecount = Config.Instance.GlobalExplosionCooldown.Value;
+                            if ((getrandom.Next((int)(800*Config.Instance.RandomnessMult.Value)) <= getrandom.Next(10)))
+                            {
+                                knockback = Config.Instance.KnockbackEnabled.Value;
+                                visible = Config.Instance.VisibleExplosions.Value;
+                                dmg = Config.Instance.MaxPlayerDamage.Value;
+                                radius = Config.Instance.Radius.Value;
+                                rand = getrandom.Next(50);
+                                where = __instance.itemProperties.positionOffset;
+                                GameNetworkManager.Instance.localPlayerController.GetComponent<HiveMindManager>().servertime(__instance.NetworkObject.transform.position.x, __instance.NetworkObject.transform.position.y, __instance.NetworkObject.transform.position.z, rand,knockback,visible,dmg,radius);
+                            }
+                        }
+                        else if (Framecount > 0)
+                        {
+                            Framecount -= 1;
+                        }
                     }
-                }
-                else if (Framecount > 0)
-                {
-                    Framecount -= 1;
                 }
             }
         }
