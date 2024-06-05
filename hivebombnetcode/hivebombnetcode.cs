b8761ade50b66fef2502799f17e1b152844a32d6
Pandemoniusly <157427865+Pandemoniusly@users.noreply.github.com>
2024-06-04 20:31:34 -0400

    Update hivebombnetcode.cs

--- hivebombnetcode/hivebombnetcode.cs
+++ hivebombnetcode/hivebombnetcode.cs
@@ -3,53 +3,74 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using System.Reflection;
+using System.IO;
+using GameNetcodeStuff;
 using UnityEngine;
+using UnityEngine.Assertions;
+using RuntimeNetcodeRPCValidator;
 
 namespace hivebombnetcode
 {
-    [BepInPlugin(modGUID, modName, modVersion)]
-//    [BepInDependency("com.rune580.LethalCompanyInputUtils", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInPlugin("Pandemonius.BeehiveBomb", "BeehiveBomb", "2.0.0")]
+    [BepInDependency(RuntimeNetcodeRPCValidator.MyPluginInfo.PLUGIN_GUID, RuntimeNetcodeRPCValidator.MyPluginInfo.PLUGIN_VERSION)]
+    //    [BepInDependency("com.rune580.LethalCompanyInputUtils", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
-        public const string modGUID = "Pandemonius.BeehiveBomb";
-        public const string modName = "BeehiveBomb";
-        public const string modVersion = "1.0.0";
 
-        private readonly Harmony harmony = new Harmony(modGUID);
+        private readonly Harmony harmony = new Harmony("Pandemonius.BeehiveBomb");
 
-        private static Plugin instance;
+        static internal Plugin instance;
 
         static internal ManualLogSource mls;
 
-        static internal ExplosionHandler explosionHandler = new ExplosionHandler();
-        public static ConfigFile BepInExConfig() { return instance.Config; }
-        static internal AssetBundle MainAssetBundle = null;
+        private NetcodeValidator netcodeValidator;
+
+        public static ConfigFile BepInExConfig()
+        {
+            return instance.Config;
+        }
+
+        public static AssetBundle bundle;
+
+        public GameObject HiveMindPrefabobj;
+
         public void Awake()
         {
             // entry point when mod load
-            if (instance == null)
-                instance = this;
+            instance = this;
 
-            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            string assetlocation = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "hivebombing");
+            bundle = AssetBundle.LoadFromFile(assetlocation);
+
+            HiveMindPrefabobj = bundle.LoadAsset<GameObject>("Assets/TheHiveMind.prefab");
+            HiveMindPrefabobj.AddComponent<HiveMindManager>();
+
+            mls = BepInEx.Logging.Logger.CreateLogSource("Pandemonius.BeehiveBomb");
+            netcodeValidator = new NetcodeValidator("Pandemonius.BeehiveBomb");
+            netcodeValidator.PatchAll();
             hivebombnetcode.Config.Instance.Setup();
-            mls.LogMessage("Plugin " + modName + " loaded!");
+            mls.LogMessage("Welcome to the HiveMind");
             harmony.PatchAll(typeof(beeupdate));
-        }
-    }
+            //harmony.PatchAll(typeof(HiveMindInterface));
+            harmony.PatchAll(typeof(HiveCreator));
 
-  //  [HarmonyPatch]
-  //  public class NetworkObjectManager
-  //  {
-  //
-  //      [HarmonyPostfix, HarmonyPatch(typeof(GameNetworkManager), nameof(GameNetworkManager.Start))]
-  //      public static void Init()
-  //      {
-  //          if (networkPrefab != null)
-   //             return;
-  //          
-  //          networkPrefab = (GameObject)MainAssetBundle.LoadAsset("ExampleNetworkHandler");
-  //      }
-  //
-  //      static GameObject networkPrefab;
-  //  }
+            netcodeValidator.BindToPreExistingObjectByBehaviour<HiveMindManager, PlayerControllerB>();
+        }
+
+        //  [HarmonyPatch]
+        //  public class NetworkObjectManager
+        //  {
+        //
+        //      [HarmonyPostfix, HarmonyPatch(typeof(GameNetworkManager), nameof(GameNetworkManager.Start))]
+        //      public static void Init()
+        //      {
+        //          if (networkPrefab != null)
+        //             return;
+        //          
+        //          networkPrefab = (GameObject)MainAssetBundle.LoadAsset("ExampleNetworkHandler");
+        //      }
+        //
+        //      static GameObject networkPrefab;
+        //  }
+    }
 }
