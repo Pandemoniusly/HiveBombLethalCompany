99606b9f51cb52a29199430354e7d4c2ce63108e
Pandemoniusly <157427865+Pandemoniusly@users.noreply.github.com>
2024-07-08 16:16:31 -0400

    Update HiveMindManager.cs

--- hivebombnetcode/HiveMindManager.cs
+++ hivebombnetcode/HiveMindManager.cs
@@ -7,27 +7,61 @@ namespace hivebombnetcode
 {
     public class HiveMindManager : NetworkBehaviour
     {
-        public override void OnNetworkSpawn()
+        [ServerRpc(RequireOwnership = false)]
+        public void PingServerRpc(ServerRpcParams rpcParams = default)
         {
-
-            if (NetworkManager.Singleton.IsHost || NetworkManager.Singleton.IsServer)
-                Instance?.gameObject.GetComponent<NetworkObject>().Despawn();
-            Instance = this;
-
-            base.OnNetworkSpawn();
-        }
-
-        [ServerRpc]
-        public void ExplodeAtServerRpc(float x, float y, float z, float rand, bool knockback, bool visible, int dmg, float rad, ServerRpcParams rpcParams = default)
-        {
-                ExplodeAtClientRpc(x,y,z, rand, knockback, visible, dmg, rad);
+            PongClientRpc();
         }
 
         [ClientRpc]
-        public void ExplodeAtClientRpc(float x, float y, float z, float rand, bool knockback, bool visible, int dmg, float rad)
+        public void PongClientRpc()
         {
-                Landmine.SpawnExplosion(new Vector3(x,y,z), visible, 0, rad, dmg, knockback ? rand : 0);
+            if (Plugin.islobbyHost) ConfigPingServerRpc(Config.Instance.KnockbackEnabled.Value, Config.Instance.VisibleExplosions.Value, Config.Instance.Radius.Value, Config.Instance.MaxPlayerDamage.Value);
         }
+
+        [ServerRpc]
+        public void ConfigPingServerRpc(bool knock, bool visible, float radius, int dmg, ServerRpcParams rpcParams = default)
+        {
+            ConfigPongClientRpc(knock, visible, radius, dmg);
+        }
+
+        [ClientRpc]
+        public void ConfigPongClientRpc(bool knock, bool visible, float radius, int dmg)
+        {
+            hivebombnetcode.Plugin.knockback = knock;
+            hivebombnetcode.Plugin.visible = visible;
+            hivebombnetcode.Plugin.radius = radius;
+            hivebombnetcode.Plugin.maxdmg = dmg;
+        }
+
+        [ServerRpc]
+        public void ExplodePingServerRpc(float x, float y, float z, int rand, ServerRpcParams rpcParams = default)
+        {
+                ExplodePongClientRpc(x,y,z,rand);
+        }
+
+        [ClientRpc]
+        public void ExplodePongClientRpc(float x, float y, float z, int rand)
+        {
+            try
+            {
+                Landmine.SpawnExplosion(new Vector3(x, y, z), hivebombnetcode.Plugin.visible, 0, hivebombnetcode.Plugin.radius, hivebombnetcode.Plugin.maxdmg, hivebombnetcode.Plugin.knockback ? rand : 0);
+            }
+            catch { }
+        }
+        private void Awake()
+        {
+            if (!IsHost)
+                StartCoroutine(WaitForSomeTime());
+        }
+
+        private IEnumerator WaitForSomeTime()
+        {
+            yield return new WaitUntil(() => NetworkObject.IsSpawned);
+
+            PingServerRpc();
+        }
+
         public static HiveMindManager Instance { get; private set; }
     }
 }
